30ab2a70efa8487a0b713be39c386cd44bd5e4f2
bdcaetano-sudo <bd.caetano@kodland.team>
2026-05-04 21:23:42 -0300

    Add files via upload
    gerador de senhas

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import random
+
+caracter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+tamanho_senha = int(input("Digite o tamanho da senha desejada"))
+senha = ''
+
+for i in range(tamanho_senha):
+    senha= senha + random.choice(caracter)
+
+print(f'A senha gerada é {senha}')
